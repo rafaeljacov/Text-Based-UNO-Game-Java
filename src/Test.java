f9377798b12e6eabf0f8fbd88257015a4508b4b8
Rafael Jacov Medel <110229646+rafaeljacov@users.noreply.github.com>
2024-08-06 08:52:55 +0800

    Delete src/Test.java

--- src/Test.java
+++
@@ -1,8 +0,0 @@
-
-public class Test {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello");
-        Thread.sleep(3000);
-        System.out.println("World");
-    }
-}
